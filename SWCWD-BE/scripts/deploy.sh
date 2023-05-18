a409c569655ff8f562b6bd9fd5b23c54dbd96c21
TProgrammer <77713508+TProgramer@users.noreply.github.com>
2023-05-19 00:29:21 +0900

    Update deploy.sh

--- SWCWD-BE/scripts/deploy.sh
+++ SWCWD-BE/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+fuser -k 80/tcp
 REPOSITORY=/home/ec2-user/BE
 cd $REPOSITORY
-sudo su
-nohup java -Dserver.port=80 -jar target/*.jar&
+nohup java -Dserver.port=80 -jar target/*.jar > $REPOSITORY/nohup.out 2>&1 &
