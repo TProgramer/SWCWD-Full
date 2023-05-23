8a88bb74695d1133d74037cdc1a21f42f55bd064
TProgramer <tposter@naver.com>
2023-05-23 09:23:27 +0900

    Vuex-persistedstate 를 통해 새로고침시에도 상태관리 유지

--- SWCWD-FE/src/views/MyPageView.vue
+++ SWCWD-FE/src/views/MyPageView.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-      <vue-inline-calendar @select-date="selectedDate = $event" />
+    <vue-inline-calendar @select-date="selectedDate = $event" :spec-min-date="new Date(`${regDate}`)"
+      :spec-max-date="new Date()" />
   </div>
 </template>
 
 <script>
 import VueInlineCalendar from 'vue2-inline-calendar'
-
+import { mapState } from 'vuex';
 export default {
-name: 'MyPageView',
-components: {
-  VueInlineCalendar
-},
-data() {
-  return {
-    selectedDate: null
+  name: 'MyPageView',
+  components: {
+    VueInlineCalendar
+  },
+  data() {
+    return {
+      selectedDate: null
+    }
+  },
+  computed: {
+    ...mapState(["regDate"]),
   }
-},
 }
 </script>
