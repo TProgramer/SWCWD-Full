9a2ba53875fdbe777c348c24b1864c12c60573af
송은수 <sksksk705@naver.com>
2023-05-25 13:30:09 +0900

    Ui

--- SWCWD-FE/src/main.js
+++ SWCWD-FE/src/main.js
@@ -1,29 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import lineClamp from 'vue-line-clamp'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import SkeletonCards from 'vue-ultimate-skeleton-cards'
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import lineClamp from 'vue-line-clamp';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import SkeletonCards from 'vue-ultimate-skeleton-cards';
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import vuetify from './plugins/vuetify'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import vuetify from './plugins/vuetify';
 
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 
-Vue.use(SkeletonCards)
+Vue.use(SkeletonCards);
 
-Vue.config.productionTip = false
-Vue.use(lineClamp)
+Vue.config.productionTip = false;
+Vue.use(lineClamp);
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
